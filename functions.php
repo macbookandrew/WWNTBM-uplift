63712340469a55cfd2708cd8ebd0296a5499c29e
Andrew Minion <andrew@andrewrminion.com>
2017-05-01 13:11:02 -0400

    add JS to handle email subscription form

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+/**
+ * Enqueue subscription form JS
+ */
+function uplift_enqueue_js() {
+	wp_enqueue_script( 'subscribe', get_stylesheet_directory_uri() . '/js/subscribe.js', array( 'jquery' ), NULL, true );
+}
+add_action( 'wp_enqueue_scripts', 'uplift_enqueue_js' );
---
+++ js/subscribe.js
@@ -0,0 +1,10 @@
+(function($){
+	$(document).ready(function(){
+		$('.textwidget .mc4wp-form').slideUp();
+
+		$('.subscribe a.fa-envelope').on('click', function(event) {
+			event.preventDefault();
+			$(this).parents('.subscribe').next('script').next('.mc4wp-form').slideToggle();
+		})
+	});
+})(jQuery);
