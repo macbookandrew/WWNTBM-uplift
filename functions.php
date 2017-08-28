973cceb269fb761d4b6c1f80e339b915c728f314
Andrew Minion <andrew@andrewrminion.com>
2017-08-28 14:57:58 -0400

    normalize whitespace

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * Enqueue subscription form JS
  */
 function uplift_enqueue_js() {
-	wp_enqueue_script( 'subscribe', get_stylesheet_directory_uri() . '/js/subscribe.js', array( 'jquery' ), NULL, true );
+    wp_enqueue_script( 'subscribe', get_stylesheet_directory_uri() . '/js/subscribe.js', array( 'jquery' ), NULL, true );
 }
 add_action( 'wp_enqueue_scripts', 'uplift_enqueue_js' );
 
@@ -12,32 +12,32 @@ add_action( 'wp_enqueue_scripts', 'uplift_enqueue_js' );
  * Make podcasts private by default
  */
 function default_post_visibility() {
-	global $post;
+    global $post;
 
-	if ( 'publish' == $post->post_status ) {
-		$visibility = 'public';
-		$visibility_trans = __( 'Public' );
-	} elseif ( !empty( $post->post_password ) ) {
-		$visibility = 'password';
-		$visibility_trans = __( 'Password protected' );
-	} elseif ( $post->post_type == 'podcast' && is_sticky( $post->ID ) ) {
-		$visibility = 'public';
-		$visibility_trans = __( 'Public, Sticky' );
-	} else {
-		$post->post_password = '';
-		$visibility = 'private';
-		$visibility_trans = __( 'Private' );
-	} ?>
+    if ( 'publish' == $post->post_status ) {
+        $visibility = 'public';
+        $visibility_trans = __( 'Public' );
+    } elseif ( !empty( $post->post_password ) ) {
+        $visibility = 'password';
+        $visibility_trans = __( 'Password protected' );
+    } elseif ( $post->post_type == 'podcast' && is_sticky( $post->ID ) ) {
+        $visibility = 'public';
+        $visibility_trans = __( 'Public, Sticky' );
+    } else {
+        $post->post_password = '';
+        $visibility = 'private';
+        $visibility_trans = __( 'Private' );
+    } ?>
 
-	<script type='text/javascript'>
-		(function($) {
-			try {
-				$('#post-visibility-display').text('<?php echo $visibility_trans; ?>');
-				$('#hidden-post-visibility').val('<?php echo $visibility; ?>');
-				$('#visibility-radio-<?php echo $visibility; ?>').attr('checked', true);
-			} catch(e) {}
-		})(jQuery);
-	</script>
-	<?php
+    <script type='text/javascript'>
+        (function($) {
+            try {
+                $('#post-visibility-display').text('<?php echo $visibility_trans; ?>');
+                $('#hidden-post-visibility').val('<?php echo $visibility; ?>');
+                $('#visibility-radio-<?php echo $visibility; ?>').attr('checked', true);
+            } catch(e) {}
+        })(jQuery);
+    </script>
+    <?php
 }
 add_action( 'post_submitbox_misc_actions' , 'default_post_visibility' );
--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -11,39 +11,39 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
 
-	<?php if ( has_post_thumbnail() ) :
-		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
+    <?php if ( has_post_thumbnail() ) :
+        $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
 
-		$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
+        $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
 
-		$thumbnail_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
+        $thumbnail_attributes = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
 
-		// Calculate aspect ratio: h / w * 100%.
-		$ratio = $thumbnail_attributes[2] / $thumbnail_attributes[1] * 100;
-		?>
+        // Calculate aspect ratio: h / w * 100%.
+        $ratio = $thumbnail_attributes[2] / $thumbnail_attributes[1] * 100;
+        ?>
 
-		<div class="panel-image" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
-			<div class="panel-image-prop" style="padding-top: <?php echo esc_attr( $ratio ); ?>%"></div>
-		</div><!-- .panel-image -->
+        <div class="panel-image" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
+            <div class="panel-image-prop" style="padding-top: <?php echo esc_attr( $ratio ); ?>%"></div>
+        </div><!-- .panel-image -->
 
-	<?php endif; ?>
+    <?php endif; ?>
 
-	<div class="panel-content">
-		<div class="wrap">
-			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+    <div class="panel-content">
+        <div class="wrap">
+            <header class="entry-header">
+                <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
-				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+                <?php twentyseventeen_edit_link( get_the_ID() ); ?>
 
-			</header><!-- .entry-header -->
+            </header><!-- .entry-header -->
 
-			<div class="entry-content">
-				<?php
-					/* translators: %s: Name of current post */
-					the_content( sprintf(
-						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-						get_the_title()
-					) );
+            <div class="entry-content">
+                <?php
+                    /* translators: %s: Name of current post */
+                    the_content( sprintf(
+                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+                        get_the_title()
+                    ) );
 
                     if ( ! is_user_logged_in() ) {
                         $login_args = array(
@@ -53,13 +53,13 @@
                         <p>Please log in to access these podcasts.</p>';
                         wp_login_form( $login_args );
                     } else {
-						echo '<h2>Podcasts</h2>
-						<p><a href="' . get_home_url() . '/podcast/" class="button">Access the podcasts here</a>.</p>';
-					}
-				?>
-			</div><!-- .entry-content -->
+                        echo '<h2>Podcasts</h2>
+                        <p><a href="' . get_home_url() . '/podcast/" class="button">Access the podcasts here</a>.</p>';
+                    }
+                ?>
+            </div><!-- .entry-content -->
 
-		</div><!-- .wrap -->
-	</div><!-- .panel-content -->
+        </div><!-- .wrap -->
+    </div><!-- .panel-content -->
 
 </article><!-- #post-## -->
