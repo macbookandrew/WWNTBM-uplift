86f5480d69e9a00328afb9d80d79f4a109465dbd
Andrew Minion <andrew@andrewrminion.com>
2018-12-11 14:56:54 -0500

    fix some wpcs issues

--- functions.php
+++ functions.php
@@ -1,55 +1,70 @@
 <?php
+/**
+ * Theme functions
+ *
+ * @package WWNTBM-uplift
+ */
+
+/**
+ * Auto-calculate and define theme version constant for use in multiple stylesheets/scripts
+ */
+define( 'UPLIFT_THEME_VERSION', wp_get_theme()->get( 'Version' ) );
 
 /**
  * Enqueue subscription form JS
  */
 function uplift_enqueue_js() {
-    wp_enqueue_script( 'subscribe', get_stylesheet_directory_uri() . '/js/subscribe.js', array( 'jquery' ), NULL, true );
+	wp_enqueue_script( 'subscribe', get_stylesheet_directory_uri() . '/js/subscribe.js', array( 'jquery' ), UPLIFT_THEME_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'uplift_enqueue_js' );
 
 /**
- * Show podcast info only to logged-in users
+ * Show podcast info only to logged-in users.
+ *
  * @return string HTML content
  */
 function uplift_login_shortcode() {
-    ob_start();
+	ob_start();
 
-    if ( ! is_user_logged_in() ) {
-        $login_args = array(
-            'redirect'  => get_home_url() . '/podcast/',
-        );
-        echo '<h2>Log In</h2>
-        <p>Please log in to access these podcasts.</p>';
-        wp_login_form( $login_args );
-    } else {
-        echo '<h2>Podcasts</h2>
-        <p><a href="' . get_home_url() . '/podcast/" class="button">Access the podcasts here</a>.</p>';
-    }
+	if ( ! is_user_logged_in() ) {
+		$login_args = array(
+			'redirect' => get_home_url() . '/podcast/',
+		);
+		echo '<h2>Log In</h2>
+		<p>Please log in to access these podcasts.</p>';
+		wp_login_form( $login_args );
+	} else {
+		echo '<h2>Podcasts</h2>
+		<p><a href="' . esc_url( get_home_url() ) . '/podcast/" class="button">Access the podcasts here</a>.</p>';
+	}
 
-    return ob_get_clean();
+	return ob_get_clean();
 }
 add_shortcode( 'conditional_login_form', 'uplift_login_shortcode' );
 
 /**
- * Force HTTPS feed stylsheet URL
- * @param  string $feed_url RSS feed stylesheet URL
- * @return string modified RSS feed stylesheet URL
+ * Force HTTPS feed stylsheet URL.
+ *
+ * @param  string $feed_style_url RSS feed stylesheet URL.
+ *
+ * @return string modified RSS feed stylesheet URL.
  */
 function uplift_rss_stylesheet( $feed_style_url ) {
-    return str_replace( 'http://', 'https://', $feed_style_url );
+	return str_replace( 'http://', 'https://', $feed_style_url );
 }
 add_filter( 'ssp_rss_stylesheet', 'uplift_rss_stylesheet' );
 
 /**
- * Add placeholder attributes to Mailchimp signup form
- * @param  array $fields form fields
- * @return array modified form fields
+ * Add placeholder attributes to Mailchimp signup form.
+ *
+ * @param  array $fields form fields.
+ *
+ * @return array modified form fields.
  */
 function uplift_mc_placeholders( $fields ) {
-    $fields[0]['default'] = 'john.doe@example.com';
-    $fields[1]['default'] = 'John';
-    $fields[2]['default'] = 'Doe';
-    return $fields;
+	$fields[0]['default'] = 'john.doe@example.com';
+	$fields[1]['default'] = 'John';
+	$fields[2]['default'] = 'Doe';
+	return $fields;
 }
 add_filter( 'mailchimp_dev_mode_fields', 'uplift_mc_placeholders' );
