32772a516a43d7399f7aeef957ed22bb4206faae
Andrew Minion <andrew@andrewrminion.com>
2018-12-11 15:02:14 -0500

    fix code style

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,10 +1,13 @@
-(function($){
-	$(document).ready(function(){
+/* global jQuery */
+'use strict';
+
+(function($) {
+	$(document).ready(function() {
 		$('.textwidget .mc4wp-form').slideUp();
 
 		$('.subscribe a.fa-envelope').on('click', function(event) {
 			event.preventDefault();
 			$(this).parents('.subscribe').next('script').next('.mc4wp-form').slideToggle();
-		})
+		});
 	});
-})(jQuery);
+}(jQuery));
